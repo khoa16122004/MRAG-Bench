aabd3ef4bf27f7b7bcd90a0274e1ddc9fcde699b
khoa16122004 <khoatr.work@gmail.com>
2025-03-25 20:39:35 +0700

    Update llava_one_vision.py

--- eval/models/llava_one_vision.py
+++ eval/models/llava_one_vision.py
@@ -118,3 +118,4 @@ if __name__ == "__main__":
 
     eval_model(args)
 
+    # python3 eval/models/llava_one_vision.py --answers-file "llava_one_vision_gt_rag_results.jsonl" --use_rag True --use_retrieved_examples False 
