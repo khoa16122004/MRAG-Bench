f4f918e6634c4ad33996d1e111410edf4fc06ead
khoa16122004 <khoatr.work@gmail.com>
2025-03-25 20:14:28 +0700

    Update save_data.py

--- eval/utils/save_data.py
+++ eval/utils/save_data.py
@@ -2,11 +2,16 @@ from dataloader import load_dataset
 from tqdm import tqdm
 from PIL import Image
 import io
+import os
 
 def extract_data(output_dir):
     mrag_bench = load_dataset("uclanlp/MRAG-Bench", split="test")
     
     for item in tqdm(mrag_bench):
+        img_dir = os.path.join(output_dir, item)
+        os.makedirs(img_dir, exist_ok=True)
+        
+        
         qs_id = item['id'] 
         qs = item['question']
         ans = item['answer']
@@ -16,9 +21,22 @@ def extract_data(output_dir):
         choices_B = item['B']
         choices_C = item['C']
         choices_D = item['D']
+        
+        with open(os.path.join(img_dir, f"question.txt"), "w") as f:
+            f.write(f"Question: {qs}\n\n")
+            f.write(f"A: {choices_A}\nB: {choices_B}\nC: {choices_C}\nD: {choices_D}\n\n")
+            f.write(f"Anwers: {gt_choice}")
+            
+        
         gt_images = item['gt_images']
         gt_images = [ib.convert("RGB") if isinstance(ib, Image.Image) else Image.open(io.BytesIO(ib['bytes'])).convert("RGB") for ib in gt_images] # retrieval image
+        for i, img in enumerate(gt_images):
+            img.save(os.path.join(img_dir, f"gt_{i}.png"))
+        
         
         image = item['image'].convert('RGB') # input-question image, 
-        print(image)
+        image.save(os.path.join(img_dir, "question_img.png"))
         break
+    
+
+extract_data("Extract_image")
