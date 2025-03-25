457cdd49621473244bdbaf5fa17846ea645151b3
khoa16122004 <khoatr.work@gmail.com>
2025-03-25 16:57:34 +0700

    Update dataloader.py

--- eval/utils/dataloader.py
+++ eval/utils/dataloader.py
@@ -43,7 +43,7 @@ def bench_data_loader(args, image_placeholder="<image>"):
             image_files = [image]
         else: 
             image_files = [image] + gt_images
-            prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly. {image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}\n"
+            prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly."
             if scenario == 'Incomplete':
                 prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly. {image_placeholder}{image_placeholder}\n"
 
