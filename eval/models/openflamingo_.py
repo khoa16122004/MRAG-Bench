bf93c317a38977800fa9d9481f08e749a04459db
khoa16122004 <khoatr.work@gmail.com>
2025-03-26 21:50:01 +0700

    Update openflamingo_.py

--- eval/models/openflamingo_.py
+++ eval/models/openflamingo_.py
@@ -30,7 +30,7 @@ class OpenFlamingo:
         generated_text = self.model.generate(
             vision_x=vision_x,
             lang_x=lang_x["input_ids"],
-            attention_mask=lang_x["attention_mask"].cuda(),
+            attention_mask=lang_x["attention_mask"],
             max_new_tokens=20,
             num_beams=3,
         )
