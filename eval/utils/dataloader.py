b947466e7aba3d1122b13c50d938bdd5b70d979f
khoa16122004 <khoatr.work@gmail.com>
2025-03-26 22:43:38 +0700

    Update dataloader.py

--- eval/utils/dataloader.py
+++ eval/utils/dataloader.py
@@ -49,21 +49,17 @@ def bench_data_loader(args, image_placeholder="<image>", special_token=None):
             image_files = [image] + gt_images
             prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly. {image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}\n"
             if special_token:
-                prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly.{special_token}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}\n"
+                prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly.{special_token}{image_placeholder}{special_token}{image_placeholder}{special_token}{image_placeholder}{special_token}{image_placeholder}{special_token}{image_placeholder}{special_token}{image_placeholder}\n"
             # prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. First, answer with the option's letter from the given choices and explain your reasoning. {image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}{image_placeholder}\n"
             if scenario == 'Incomplete':
                 prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly. {image_placeholder}{image_placeholder}\n"
                 # prompt = f"You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. First, answer with the option's letter from the given choices. Then, explain your reasoning.. {image_placeholder}{image_placeholder}\n"
-
-        if special_token:
-            qs += f"\n Choices:\nA: {choices_A}\nB: {choices_B}\nC: {choices_C}\nD: {choices_D}"
+    
+        qs += f"\n Choices:\nA: {choices_A}\nB: {choices_B}\nC: {choices_C}\nD: {choices_D}"
         prompt_question_part = qs
         prompt_instruction_part = prompt
-        if special_token:
-            qs = prompt + special_token + qs
-        else:
-            qs = prompt + qs
-            
+        qs = prompt + qs
+        
         if args.use_rag: 
             if args.use_retrieved_examples:
                 retrieved_images = item['retrieved_images']
