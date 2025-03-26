b47bce3ae9e1382b5a44e83fa79f0b308ad13631
khoa16122004 <khoatr.work@gmail.com>
2025-03-26 09:29:55 +0700

    update

--- eval/models/main.py
+++ eval/models/main.py
@@ -5,10 +5,6 @@ import json
 from tqdm import tqdm
 import shortuuid
 
-from llava.model.builder import load_pretrained_model
-from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
-from llava.conversation import conv_templates, SeparatorStyle
 
 from PIL import Image
 import requests
