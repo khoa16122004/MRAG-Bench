50fc4edbcb95f947fe8a1a6377f658583388ea3f
khoa16122004 <khoatr.work@gmail.com>
2025-03-26 22:19:10 +0700

    Update openflamingo_.py

--- eval/models/openflamingo_.py
+++ eval/models/openflamingo_.py
@@ -33,7 +33,7 @@ class OpenFlamingo:
             max_new_tokens=20,
             num_beams=3,
         )
-        output = self.tokenizer.decode(generated_text[0])
+        output = self.tokenizer.decode(generated_text[0], skip_special_tokens=True).strip()
         return output
 
         
