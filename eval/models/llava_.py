0d96c355ee9199c8cf89380e69d1884a46df1e10
khoa16122004 <khoatr.work@gmail.com>
2025-03-27 21:07:01 +0700

    Update llava_.py

--- eval/models/llava_.py
+++ eval/models/llava_.py
@@ -41,7 +41,7 @@ class LLava:
         for batch_img in img_files:
             batch_img_tensor = process_images(batch_img, self.image_processor, self.model.config)
             batch_img_tensor = [_image.to(dtype=torch.float16, device=self.device) for _image in batch_img_tensor]
-            batch_img_sizes = [image.size for image in img_files]
+            batch_img_sizes = [image.size for image in batch_img]
             
             image_tensors.append(batch_img_tensor)
             image_sizes.append(batch_img_sizes)
