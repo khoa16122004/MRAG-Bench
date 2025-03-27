252426cab4fed97c517fab4d240632d84c8593e4
khoa16122004 <khoatr.work@gmail.com>
2025-03-27 10:59:58 +0700

    Update openflamingo_.py

--- eval/models/openflamingo_.py
+++ eval/models/openflamingo_.py
@@ -34,7 +34,7 @@ class OpenFlamingo:
             vision_x=vision_x,
             lang_x=lang_x["input_ids"].cuda(),
             attention_mask=lang_x["attention_mask"].cuda(),
-            max_new_tokens=20,
+            max_new_tokens=4096,
             pad_token_id=50277,
             num_beams=3,
         )
