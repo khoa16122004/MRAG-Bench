36a9b3d9115609b33d69ab47ff79d8d883608be3
khoa16122004 <khoatr.work@gmail.com>
2025-03-29 19:28:22 +0700

    Update attack.py

--- eval/models/attack.py
+++ eval/models/attack.py
@@ -79,7 +79,8 @@ import torch.nn.functional as F
 sim_model = SentenceTransformer('all-MiniLM-L6-v2')
 def FreeText_benchmark(args, image_tensors, input_ids, image_sizes, 
                        gt_answer, pertubation_list, model):
-    adv_img_tensors = torch.clip(image_tensors + pertubation_list).cuda()
+    
+    adv_img_tensors = [image_tensors[i] + pertubation_list[i] for i in range(len(image_tensors))]
     adv_pil_images = model.decode_image_tensors(adv_img_tensors)
     output = model.inference(input_ids, adv_img_tensors, image_sizes)[0]
     
