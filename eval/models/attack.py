f880552bde5721723ec1866a48727318c6d71837
khoa16122004 <khoatr.work@gmail.com>
2025-03-29 19:13:24 +0700

    Revert "Update attack.py"
    This reverts commit 486c6c525fb8ad63272779464cad098a0dddaf20.

--- eval/models/attack.py
+++ eval/models/attack.py
@@ -79,6 +79,7 @@ import torch.nn.functional as F
 sim_model = SentenceTransformer('all-MiniLM-L6-v2')
 def FreeText_benchmark(args, image_tensors, input_ids, image_sizes, 
                        gt_answer, pertubation_list, model):
+    to_pil = transforms.ToPILImage()
     adv_img_tensors = torch.clip(image_tensors + pertubation_list, 0, 1).cuda()
     adv_pil_images = model.decode_image_tensors(adv_img_tensors)
     
