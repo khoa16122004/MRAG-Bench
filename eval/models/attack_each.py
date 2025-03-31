b0954cb2b2de0c4bb629164c2acb546762a52d2b
khoa16122004 <khoatr.work@gmail.com>
2025-03-31 16:16:52 +0700

    Update attack_each.py

--- eval/models/attack_each.py
+++ eval/models/attack_each.py
@@ -78,14 +78,14 @@ def FreeText_benchmark(args, image_tensors, index_attack, input_ids, image_sizes
     emb1 = sim_model.encode(output, convert_to_tensor=True)
     emb2 = sim_model.encode(gt_answer, convert_to_tensor=True)
     similarity = F.cosine_similarity(emb1.unsqueeze(0), emb2.unsqueeze(0)).item()
-    s1 = 0.5 - similarity
+    s1 = 0.3 - similarity
     
     # BLEU score
     bleu = sentence_bleu([gt_answer.split()], output.split())
-    s2 = 0.5 - bleu
+    s2 = 0.3 - bleu
     
     # weighted sum
-    final_score = 0.5 * s1 + 0.5 * s2
+    final_score = s1 + s2
     return final_score, adv_pil_images, output
         
     
