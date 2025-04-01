d10aa9e045cb45c71c4f056bb5f3baa1f812e971
khoa16122004 <khoatr.work@gmail.com>
2025-04-01 12:53:43 +0700

    Update test_script.py

--- eval/models/test_script.py
+++ eval/models/test_script.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 
 model = LLava("llava-onevision-qwen2-7b-ov", "llava-onevision-qwen2-7b-ov") 
-img_path_adv = "/data/elo/khoatn/MRAG-Bench/eval/models/ES_lambda=50_epsilon=0.1_maxiter=30_pretrained=llava-onevision-qwen2-7b-ov/2/0/adv.png"
+img_path_adv = "/data/elo/khoatn/MRAG-Bench/eval/models/ES_lambda=50_epsilon=0.05_maxiter=30_pretrained=llava-onevision-qwen2-7b-ov/2/0/adv.png"
 clean_img_path = r"/data/elo/khoatn/MRAG-Bench/eval/models/ES_lambda=50_epsilon=0.1_maxiter=30_pretrained=llava-onevision-qwen2-7b-ov/2/clean_img"
 img_paths = [os.path.join(clean_img_path, f"{i}.png") for i in range(4)]
 img_files_clean = [Image.open(img_path).convert("RGB") for img_path in img_paths]
@@ -14,8 +14,8 @@ img_files_clean[1] = img_files_adv
 
 for i, img in enumerate(img_files_clean):
     img.save(f"test_{i}.png")
-
-input_ids, image_tensors, image_sizes = model.repair_input("question", img_files_clean)
+question = "Which images depict activities that are typically seasonal, and what seasons are they most associated with?<image><image><image><image>"
+input_ids, image_tensors, image_sizes = model.repair_input(question, img_files_clean)
 print(model.inference(input_ids, image_tensors, image_sizes))
 
 
