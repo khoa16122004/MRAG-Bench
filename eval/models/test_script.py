dd40f33c77cf8c9caf54b2c4d2f3ed4481d1e9c0
khoa16122004 <khoatr.work@gmail.com>
2025-04-01 14:29:18 +0700

    Update test_script.py

--- eval/models/test_script.py
+++ eval/models/test_script.py
@@ -1,10 +1,11 @@
 from llava_ import LLava
 import os
 from PIL import Image
+import torch
 
 model = LLava("llava-onevision-qwen2-7b-ov", "llava-onevision-qwen2-7b-ov") 
-img_path_adv = "/data/elo/khoatn/MRAG-Bench/eval/models/ES_lambda=50_epsilon=0.05_maxiter=30_pretrained=llava-onevision-qwen2-7b-ov/2/0/adv.png"
-clean_img_path = r"/data/elo/khoatn/MRAG-Bench/eval/models/ES_lambda=50_epsilon=0.1_maxiter=30_pretrained=llava-onevision-qwen2-7b-ov/2/clean_img"
+img_path_adv = "/data/elo/khoatn/MRAG-Bench/eval/models/test_stt_ES_lambda=50_epsilon=0.1_maxiter=2_pretrained=llava-onevision-qwen2-7b-ov/2/0.png"
+clean_img_path = r"/data/elo/khoatn/MRAG-Bench/eval/models/test_stt_ES_lambda=50_epsilon=0.1_maxiter=2_pretrained=llava-onevision-qwen2-7b-ov/2/clean_img"
 img_paths = [os.path.join(clean_img_path, f"{i}.png") for i in range(4)]
 img_files_clean = [Image.open(img_path).convert("RGB") for img_path in img_paths]
 # tmp_adv = img_files_clean[1]
@@ -18,6 +19,8 @@ for i, img in enumerate(img_files_clean):
     img.save(f"test_{i}.png")
 question = "Which images depict activities that are typically seasonal, and what seasons are they most associated with?<image><image><image><image>"
 input_ids, image_tensors, image_sizes = model.repair_input(question, img_files_clean)
-print(model.inference(input_ids, image_tensors, image_sizes))
+print("Decoded input: ", model.inference(input_ids, image_tensors, image_sizes))
 
+image_tensors = torch.load("/data/elo/khoatn/MRAG-Bench/eval/models/test_stt_ES_lambda=50_epsilon=0.1_maxiter=2_pretrained=llava-onevision-qwen2-7b-ov/2/0/all_adv.pt")
+print("Directed input: ", model.inference(input_ids, image_tensors, image_sizes))
 
